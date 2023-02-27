1808717118cca2d07db0d7bfb4f2e0dcd4dbb590
Storisi <stiivenmendez16@gmail.com>
2023-02-26 23:23:53 -0500

    dist

---
+++ assets/useDispatch.f6930254.js
@@ -0,0 +1,1 @@
+import{f as e,g as u,r as c}from"./index.89339c77.js";function r(t=e){const o=t===e?u:()=>c.exports.useContext(t);return function(){const{store:s}=o();return s}}const a=r();function i(t=e){const o=t===e?a:r(t);return function(){return o().dispatch}}const x=i();export{x as u};
